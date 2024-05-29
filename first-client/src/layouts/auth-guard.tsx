b63661e6d76d82ca2c03a1800a683025b8775eb9
kaledsds <doiybikaled@gmail.com>
2024-05-29 15:42:30 +0100

    category

--- first-client/src/layouts/auth-guard.tsx
+++ first-client/src/layouts/auth-guard.tsx
@@ -8,19 +8,5 @@ interface AuthGuardProps {
 
 export const AuthGuard: React.FC<AuthGuardProps> = ({ children }) => {
   const { isLoggedIn } = useAuth();
-  // const { currentUser } = useAuth();
-  // if (currentUser?.category === null) {
-  //   return <Navigate to="/" />;
-  // }
-  // if (currentUser?.category === "admin") {
-  //   return <Navigate to="/admin" />;
-  // }
-  // if (currentUser?.category === "teacher") {
-  //   return <Navigate to="/teacher" />;
-  // }
-  // if (currentUser?.category === "stuff") {
-  //   return <Navigate to="/personnel" />;
-  // }
-
   return isLoggedIn ? children : <Navigate to="/login" />;
 };
