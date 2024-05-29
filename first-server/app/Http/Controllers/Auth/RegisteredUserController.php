fc436e111ce5e96edc967ccd81baec471e6db617
Bilel Ben Hamza <bilelbenhamza147@gmail.com>
2024-05-29 14:35:36 +0100

    Migration

--- first-server/app/Http/Controllers/Auth/RegisteredUserController.php
+++ first-server/app/Http/Controllers/Auth/RegisteredUserController.php
@@ -21,13 +21,22 @@ class RegisteredUserController extends Controller
     public function store(Request $request): Response
     {
         $request->validate([
-            'name' => ['required', 'string', 'max:255'],
+
+            'first_name' => ['required', 'string', 'max:255'],
+            'last_name'=> ['required', 'string','max:255'],
+            'birthday'=> ['required', 'date'],
+            'phone'=> ['required', 'integer'],
+            'adress'=> ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:' . User::class],
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
         ]);
 
         $user = User::create([
-            'name' => $request->name,
+            'first_name' => $request->first_name,
+            'last_name'=> $request->last_name,
+            'birthday'=> $request->birthday,
+            'phone'=> $request->phone,
+            'adress'=> $request->adress,
             'email' => $request->email,
             'password' => Hash::make($request->string('password')),
         ]);
