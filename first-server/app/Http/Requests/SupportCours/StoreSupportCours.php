c7dab90c7d9a1e902402f6cbed8b6c78a9802645
Bilel Ben Hamza <bilelbenhamza147@gmail.com>
2024-05-30 12:19:44 +0100

    Models

--- first-server/app/Http/Requests/SupportCours/StoreSupportCours.php
+++ first-server/app/Http/Requests/SupportCours/StoreSupportCours.php
@@ -11,7 +11,7 @@ class StoreSupportCours extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
--- first-server/app/Http/Requests/SupportCours/UpdateSupportCours.php
+++ first-server/app/Http/Requests/SupportCours/UpdateSupportCours.php
@@ -11,7 +11,7 @@ class UpdateSupportCours extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
